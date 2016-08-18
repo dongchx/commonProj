57f9ed2a0fe1f81eeffd900635847f7cdafe676d
dongchx <dongchx90@gmail.com>
2016-08-18 11:48:06 +0800

    init

---
+++ commonProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  commonProj
+//
+//  Created by dongchx on 8/18/16.
+//  Copyright © 2016 dongchx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ commonProj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  commonProj
+//
+//  Created by dongchx on 8/18/16.
+//  Copyright © 2016 dongchx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
