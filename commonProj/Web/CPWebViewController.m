950d9bd59a092f52a05c33baf67d7e8c22d05a98
dongcx01 <dongcx01@fenbi.com>
2019-12-12 20:09:28 +0800

    WKWebView

--- commonProj/Web/CPWebViewController.m
+++ commonProj/Web/CPWebViewController.m
@@ -7,20 +7,44 @@
 //
 
 #import "CPWebViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface CPWebViewController () <UIWebViewDelegate>
-@property (nonatomic, strong) UIWebView *webView;
+@interface CPWebViewController () <UIWebViewDelegate, WKUIDelegate, WKNavigationDelegate>
+//@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UIBarButtonItem *redBtn;
+@property (nonatomic, strong) UIBarButtonItem *greBtn;
 @end
 
 @implementation CPWebViewController
+
+- (void)dealloc {
+    [NSNotificationCenter.defaultCenter removeObserver:self];
+}
 
 - (instancetype)init
 {
     if (self = [super init]) {
         [self setupSubviews:self.view];
+        [self registerNotifications];
     }
     
     return self;
+}
+
+- (void)registerNotifications {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didEnterBackground)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+//    [[NSNotificationCenter defaultCenter] addObserver:self
+//                                             selector:@selector(willEnterForeground)
+//                                                 name:UIApplicationWillEnterForegroundNotification
+//                                               object:nil];
+}
+
+- (void)didEnterBackground {
+    [self handleRedBtnAction:nil];
 }
 
 - (void)viewDidLoad
@@ -37,21 +61,78 @@
 
 - (void)setupSubviews:(UIView *)parentView
 {
-    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, parentView.viewWidth, parentView.viewHeight)];
-    [parentView addSubview:_webView];
-    _webView.backgroundColor = UIColor.whiteColor;
-    
-    self.webView.scrollView.scrollEnabled = NO;
-    
-    NSString *Str = [NSString stringWithFormat:@"<iframe frameborder=\"0\" width=\"359\" height=\"200\" src=\"//m.baidu.com\" allowfullscreen></iframe>"];
-    
-    [_webView loadHTMLString:Str baseURL:nil];
+    parentView.backgroundColor = UIColor.whiteColor;
+//    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, parentView.viewWidth, parentView.viewHeight)];
+//    [parentView addSubview:_webView];
+//    _webView.backgroundColor = UIColor.whiteColor;
+//
+//    self.webView.scrollView.scrollEnabled = NO;
+//
+//    NSString *Str = [NSString stringWithFormat:@"<iframe frameborder=\"0\" width=\"359\" height=\"200\" src=\"//m.baidu.com\" allowfullscreen></iframe>"];
+//
+//    [_webView loadHTMLString:Str baseURL:nil];
+    WKWebViewConfiguration *config = [WKWebViewConfiguration new];
+    config.allowsInlineMediaPlayback = YES;
+    config.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
 
+    _webView = [[WKWebView alloc] initWithFrame:CGRectZero configuration:config];
+    _webView.UIDelegate = self;
+    _webView.navigationDelegate = self;
+    _webView.backgroundColor = [UIColor clearColor];
+    _webView.opaque = NO;
+    _webView.hidden = NO;
+    _webView.scrollView.bounces = NO;
+    _webView.frame = parentView.bounds;
+    if (@available(iOS 11.0, *)) {
+        _webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
+    [parentView addSubview:_webView];
+    
+    // btns
+    _redBtn = [[UIBarButtonItem alloc] initWithTitle:@"red"
+                                               style:UIBarButtonItemStylePlain
+                                              target:self
+                                              action:@selector(handleRedBtnAction:)];
+//    _redBtn.frame = CGRectMake(20, 100, 50, 50);
+//    _redBtn.backgroundColor = UIColor.redColor;
+//    [_redBtn addTarget:self
+//                action:@selector(handleRedBtnAction:)
+//      forControlEvents:UIControlEventTouchUpInside];
+    
+    _greBtn = [[UIBarButtonItem alloc] initWithTitle:@"gre"
+                                               style:UIBarButtonItemStylePlain
+                                              target:self
+                                              action:@selector(handleGreBtnAction:)];
+    self.navigationItem.rightBarButtonItems = @[_redBtn, _greBtn];
+//    _greBtn.frame = CGRectMake(100, 100, 50, 50);
+//    _greBtn.backgroundColor = UIColor.greenColor;
+//    [_greBtn addTarget:self
+//                action:@selector(handleGreBtnAction:)
+//      forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)fullScreen
-{
+- (void)reload {
+    NSURL *url = [NSURL URLWithString:@"http://m.baidu.com"];
+    NSURL *burl = [NSURL URLWithString:@"about:blank"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLRequest *breq = [NSURLRequest requestWithURL:burl];
     
+    [_webView loadRequest:breq];
+    [_webView loadRequest:request];
+}
+
+- (void)handleRedBtnAction:(id)sender {
+    _webView.hidden = YES;
+    [_webView removeFromSuperview];
+    [self reload];
+}
+
+- (void)handleGreBtnAction:(id)sender {
+    _webView.hidden = NO;
+    if (!_webView.superview) {
+        [self.view addSubview:_webView];
+    }
+    [self reload];
 }
 
 #pragma mark - webview delegate
--- commonProjTests/commonProjTests.m
+++ commonProjTests/commonProjTests.m
@@ -36,4 +36,42 @@
     }];
 }
 
+- (void)testAssert
+{
+    // 1. Unconditional Fail：无条件失败当直接到达特定的代码分支指示失败时使用。
+    XCTFail(@"无条件失败....");
+
+    // 2.Boolean Tests
+    BOOL a = NO;
+    XCTAssert(a,@"失败时提示：a == false");
+    XCTAssertTrue(a,@"失败时提示：a == false");
+    XCTAssertFalse(a,@"失败时提示：a == true");
+
+    // 3.基础数据类型
+    NSInteger b = 1;
+    NSInteger c = 1;
+    NSInteger d = 2;
+    XCTAssertEqual(b, c, @"失败时提示：b!= c");
+    XCTAssertGreaterThan(d, c,@"失败时提示：d < c");
+    XCTAssertEqualWithAccuracy(c, d, 1,@"失败时提示：c和d的误差的绝对值大于1");
+
+    // 4.对象类型
+    NSString *nameA = @"nameA";
+    NSString *nameB = @"nameB";
+    XCTAssertEqualObjects(nameA, nameB,@"失败时提示：nameA != nameB");
+    XCTAssertNil(nameA,@"失败时提示：nameA != nil");
+    
+    // 5. Exception Tests
+    NSArray *array = @[];
+    XCTAssertThrows(array[0],@"失败时提示：array[0]没有抛出异常");
+    XCTAssertNoThrow(array[0],@"失败时提示：array[0]抛出异常");
+    XCTAssertThrowsSpecific(array[0], NSException,@"失败时提示：array[0]没有抛出NSException异常");
+    XCTAssertThrowsSpecificNamed(array[0],
+                                 NSException,
+                                 @"NSRangeException",
+                                 @"失败时提示：array[0]没有抛出名为NSRangeException的NSException异常");
+}
+
+
+
 @end
